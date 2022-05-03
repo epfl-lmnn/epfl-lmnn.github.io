b0feb88b8e96713c8d7ba74b0bea876e117a985d
YuJieWU99 <y.wu@epfl.ch>
2022-05-03 14:52:58 +0200

    Update input.js

--- scripts/input.js
+++ scripts/input.js
@@ -59,20 +59,13 @@ function updateChart() {
     return cost;
 }
 
-let btn = document.getElementById("submit");
-btn.addEventListener('click', event => {
-    cost = myChart.data.datasets[0].data.at(-1);
+var treeArea = 0
+var lifeTime = 0;
+var consume = 0;
+document.querySelector(".area").innerText = treeArea;
+document.querySelector(".time").innerText = lifeTime;
+document.querySelector(".electri").innerText = consume;
 
-})
-let treeArea = 100;
-let lifeTime = 50;
-let consume = 1;
-document.querySelector(".area").innerText=treeArea;
-document.querySelector(".time").innerText=lifeTime;
-document.querySelector(".electri").innerText=consume;
-const threshold=500;
-const energy=600;
-if (energy<threshold) {
     var treeAni = document.getElementById("tree");
     var img2 = document.createElement("img");
     img2.src = "scripts/fulltree.gif";
@@ -80,27 +73,58 @@ if (energy<threshold) {
     var lifeAni = document.getElementById("life");
     var img4 = document.createElement("img");
     img4.src = "scripts/playing.gif";
-    img4.style.width= "300px";
+    img4.style.width = "300px";
     lifeAni.appendChild(img4);
     var houseAni = document.getElementById("household");
     var img6 = document.createElement("img");
     img6.src = "scripts/fewhouse.gif";
-    img6.style.width= "350px";
+    img6.style.width = "350px";
     houseAni.appendChild(img6);
-}
-else{
-    var treeAni = document.getElementById("tree");
-    var img1 = document.createElement("img");
-    img1.src = "scripts/middletree.gif";
-    treeAni.appendChild(img1);
-    var lifeAni = document.getElementById("life");
-    var img3 = document.createElement("img");
-    img3.src = "scripts/working.gif";
-    img3.style.width= "300px";
-    lifeAni.appendChild(img3);
-    var houseAni = document.getElementById("household");
-    var img5 = document.createElement("img");
-    img5.src = "scripts/manyhouses.gif";
-    img5.style.width= "350px";
-    houseAni.appendChild(img5);
-}
+
+
+const BTN = document.getElementById('submit');
+
+BTN.addEventListener('click', () => {
+
+        treeArea = updateChart() / 2000;
+        lifeTime = updateChart() / 1000;
+        consume = updateChart() / 100000;
+        document.querySelector(".area").innerText = treeArea;
+        document.querySelector(".time").innerText = lifeTime;
+        document.querySelector(".electri").innerText = consume;
+        if (treeArea < threshold) {
+            treeAni = document.getElementById("tree");
+            img2 = document.createElement("img");
+            img2.src = "scripts/fulltree.gif";
+            treeAni.appendChild(img2);
+            lifeAni = document.getElementById("life");
+            img4 = document.createElement("img");
+            img4.src = "scripts/playing.gif";
+            img4.style.width = "300px";
+            lifeAni.appendChild(img4);
+            houseAni = document.getElementById("household");
+            img6 = document.createElement("img");
+            img6.src = "scripts/fewhouse.gif";
+            img6.style.width = "350px";
+            houseAni.appendChild(img6);
+        } else {
+            treeAni = document.getElementById("tree");
+            img1 = document.createElement("img");
+            img1.src = "scripts/middletree.gif";
+            treeAni.appendChild(img1);
+            lifeAni = document.getElementById("life");
+            img3 = document.createElement("img");
+            img3.src = "scripts/working.gif";
+            img3.style.width = "300px";
+            lifeAni.appendChild(img3);
+            houseAni = document.getElementById("household");
+            img5 = document.createElement("img");
+            img5.src = "scripts/manyhouses.gif";
+            img5.style.width = "350px";
+            houseAni.appendChild(img5);
+        }
+
+});
+
+
+
